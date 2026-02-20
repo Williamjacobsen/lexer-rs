22317ce6f682f314654ae5767f94701fdf63adad
username <villi05.v.j@gmail.com>
2026-02-20 13:17:08 +0100

    very basic lexer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 enum Token {
     Print,
     LeftParen,
@@ -5,29 +6,68 @@ enum Token {
     String(String),
     Int(i64),
     SemiColon,
+    Illegal,
 }
 
-struct Lexer<'a> {
+pub struct Lexer<'a> {
     input: &'a str,
+    current_char: u8,
+    position: usize,
 }
 
 impl<'a> Lexer<'a> {
     pub fn new(input: &'a str) -> Self {
-        let mut lexer = Lexer { input };
+        let mut lexer = Lexer {
+            input,
+            current_char: input.as_bytes()[0],
+            position: 0,
+        };
 
-        return lexer;
+        lexer
     }
 
-    pub fn next_token() -> Token {
-        let token = Token::SemiColon;
+    fn advance(&mut self) {
+        self.position += 1;
+        if self.position < self.input.len() {
+            self.current_char = self.input.as_bytes()[self.position];
+        }
+    }
+
+    pub fn next_token(&mut self) -> Token {
+        let token = match self.current_char {
+            b' ' => {
+                println!("Whitespace");
+                Token::Illegal // temporary
+            }
+            b'(' => Token::LeftParen,
+            b')' => Token::RightParen,
+            b';' => Token::SemiColon,
+            b'"' => {
+                // Consume string,
+                Token::Illegal // temporary
+            }
+            b'0'..=b'9' => {
+                // Consume integer,
+                Token::Illegal // temporary
+            }
+            //b'a'..=b'z' | b'A'..='Z' | b'_' => {
+            //    // Consume identifier
+            //}
+            _ => Token::Illegal,
+        };
+
+        self.advance();
+
         return token;
     }
 }
 
 fn main() {
-    println!("Hello, world!");
-
     let input = r#"Print("Test");"#;
 
     let mut lexer = Lexer::new(input);
+
+    for char in input.chars() {
+        println!("{:?}", lexer.next_token());
+    }
 }
